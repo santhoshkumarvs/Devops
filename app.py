c6a89796314812082b54d5ae4c42a45949202f82
santhosh <santhoshkumarvs437@gmail.com>
2019-09-10 21:11:10 +0530

    three print statements created

--- app.py
+++ app.py
@@ -10,14 +10,5 @@ print("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod 
 dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
 pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
 print("Needed improvement")
-
-
-
-
-
-
-
-
-
-
-
+print("Needed improvement-1")
+print("Needed improvement-2")
