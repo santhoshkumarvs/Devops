5cdd4e8f3ac87779c6dffb2dbf5d84eb8e764b55
root <root@ip-10-1-2-89.ec2.internal>
2019-09-10 14:41:08 +0000

    chk with feature branch

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 print(This is the" devops-program")
 
-
-
-
-
-
-=======
 print("Linux AMI created")
->>>>>>> 9785c41588a51a80cfccc270ed3bd30f344bdee3
-print("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+print("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
 print("Needed improvement")
+
+
+
+
+
+
+
+
+
+
+
