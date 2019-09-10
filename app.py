e7456a4d7aafcd6635c14bf71e7a44114f02ccf2
santhosh <santhoshkumarvs437@gmail.com>
2019-09-10 21:46:08 +0530

    added ami ununtu statement

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ print(This is the" devops-program")
 print("Everything is fine")
 print("Redhat AMI created")
 print("suse AMI created")
-
+print("ubuntu AMI created") 
 print("Linux AMI created")
 print("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
 dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
